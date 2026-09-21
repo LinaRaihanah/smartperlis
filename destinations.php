62b21dff89594a282552df214a495dab5b42a744
YashiniJayeseelan <yashinidevi1@gmail.com>
2026-09-21 14:19:58 +0800

    1w2

--- destinations.php
+++ destinations.php
@@ -17,7 +17,7 @@ include("config.php");
         content="width=device-width, initial-scale=1.0">
 
     <title>
-        Destinations - PERLIS TOURISM SMART PORTAL
+        Explore - PERLIS TOURISM SMART PORTAL
     </title>
 
 
